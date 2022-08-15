cf32020cf33235c5346a1e0f568ec8789c1236ff
Sebastian-Falk Birchner <Sebastian-Falk.Birchner@promos-consult.de>
2022-08-15 10:37:07 +0200

    Initial Commit

---
+++ Questionnaire/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Questionnaire
+//
+//  Created by Sebastian-Falk Birchner on 15.08.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Questionnaire/QuestionnaireApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionnaireApp.swift
+//  Questionnaire
+//
+//  Created by Sebastian-Falk Birchner on 15.08.22.
+//
+
+import SwiftUI
+
+@main
+struct QuestionnaireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
